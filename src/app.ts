d010bff1eb324cb89bac090341f75c32803d3a2c
Vadim_Eryomin <vadim.eremin.2005@mail.ru>
2026-04-07 21:29:37 +0600

    feat: creating recipe - uploading images and showig them in app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'nativescript-vue';
 import Home from './components/Home.vue';
 
+import { init } from '@nativescript/background-http';
+init()
+
 createApp(Home).start();
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,24 @@
 <script lang="ts" setup>
+import { Label } from '@nativescript/core';
 import {
   ref,
   computed,
   onMounted,
   onUnmounted,
   $navigateTo,
+  ActionBar,
 } from 'nativescript-vue';
+import RecipeMake from './RecipeMake.vue';
 
 </script>
 
 <template>
   <Frame>
-    <Page>
-      
-    </Page>
+    <RecipeMake />
+    
   </Frame>
 </template>
 
+<!-- android-elevation & android-dynamic-elevation-offset: 0 -->
 <style>
 </style>
